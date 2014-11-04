4183237f07af8f57eb8a83931efb49134205723a
sherifnada <snadalive@gmail.com>
2014-11-04 13:55:15 -0500

    updated style

---
+++ snada_string_ops.py
@@ -0,0 +1,28 @@
+"""
+A python utilities library that helps me in my day to day work. 
+
+Author: Sherif Nada, 2014
+"""
+
+
+def two_d_list_as_table(two_dimensional_list):
+	"""
+	Takes a two-dimensional list and returns it in a table format that is easy
+	to import into MS word/excel. 
+	
+	Args: 
+		twoDimensionalList - 2D list
+
+	Returns: 
+		A string
+	"""
+
+	print "----------------------------------------"
+	my_pretty_string = ''
+	for row in range(len(two_dimensional_list)):
+		for item in range(len(two_dimensional_list[row])):
+			my_pretty_string += str(two_dimensional_list[row][item]) + "\t"
+
+		my_pretty_string += '\n'
+
+	return my_pretty_string
